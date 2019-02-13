58bf47fe2e66f38e2d4da332da79fd72c849e320
Maksim_Karneyeu <maksim_karneyeu@epam.com>
2019-02-13 21:57:18 +0300

    add angular

---
+++ 7.1.Angular_part1/News/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ResultListComponent } from './result-list/result-list.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { ResultListItemComponent } from './result-list-item/result-list-item.component';
+import { NewsDetailsComponent } from './news-details/news-details.component';
+import { NewsFormComponent } from './news-form/news-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ResultListComponent,
+    ToolbarComponent,
+    ResultListItemComponent,
+    NewsDetailsComponent,
+    NewsFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ 7.1.Angular_part1/News/src/app/result-list/result-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-body',
+  templateUrl: './result-list.component.html',
+  styleUrls: ['./result-list.component.css']
+})
+export class ResultListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
