62f0e2ed5dbc71adfde1473915153d2f4d31ad50
Maksim Karneyeu <maksim_karneyeu@epam.com>
2019-02-26 18:13:59 +0300

    update reactive forms

--- 7.2.Angular_part2/src/app/news-details/news-details.component.ts
+++ 7.2.Angular_part2/src/app/news-details/news-details.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { DataService } from '../data.service';
-import { News } from '../news';
 import { DetailsParams } from '../DetailsParams';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Guid } from '../creat-guid';
-import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'news-details',
@@ -12,39 +11,55 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class NewsDetailsComponent implements OnInit {
   @Input() details: DetailsParams;
-
+  
+  public id: FormControl = new FormControl(Guid.newGuid());
+  public name: FormControl = new FormControl('', Validators.required);
+  public description: FormControl = new FormControl('');
+  public author: FormControl = new FormControl('');
+  public url: FormControl = new FormControl('');
+  public urlToImage: FormControl = new FormControl('');
+  public publishedAt: FormControl = new FormControl();
+  public content: FormControl = new FormControl('');
   public editForm = new FormGroup({
-    name: new FormControl(''),
-    description: new FormControl(''),
-    author: new FormControl(''),
-    url: new FormControl(''),
-    urlToImage: new FormControl(''),
-    publishedAt: new FormControl(''),
-    content: new FormControl(''),
+    id: this.id,    
+    name: this.name,
+    description: this.description,
+    author: this.author,
+    url: this.url,
+    urlToImage: this.urlToImage,
+    publishedAt: this.publishedAt,
+    content: this.content
   });
-
 
   constructor(private dataService: DataService) {
   }
 
-  public onSubmit() {
-    alert(this.editForm.value);
+  public onSubmit() {  
     switch (this.details.detailsType) {
-      case 'Create':
-      //  this.dataService.addNews(this.editForm.value);
+      case "Create":
+        this.dataService.addNews(this.editForm.value);          
         alert('News has been created!');
         break;
-      case 'Edit':
-     //   this.dataService.updateNews(this.model.source.id, this.model);
-        alert('News has been updated!');
+      case "Edit":        
+        this.dataService.updateNews(this.editForm.value.id, this.editForm.value);  
+        alert('News has been updated!');        
         break;
     }
   }
 
   ngOnInit() {
     this.dataService.changeSource(this.details.title);
+
     if (this.details.id) {
-    //  this.model = this.dataService.getNewsById(this.details.id);
+      let editable = this.dataService.getNewsById(this.details.id);
+      this.id.setValue(editable.source.id);
+      this.name.setValue(editable.source.name);
+      this.description.setValue(editable.description);
+      this.author.setValue(editable.author);
+      this.url.setValue(editable.url);
+      this.urlToImage.setValue(editable.urlToImage);
+      this.publishedAt.setValue(editable.publishedAt);
+      this.content.setValue(editable.content);
     }
   }
 }
